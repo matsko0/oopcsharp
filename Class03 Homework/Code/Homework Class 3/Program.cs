7eeb938dc337e22433c34bf26773e1ce6570ba5c
matsko0 <aleksandrov800@gmail.com>
2025-03-11 22:50:55 +0100

    homework03

---
+++ Class03 Homework/Code/Homework Class 3/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//Make a console application called SumOfEven.
+//Inside it create an array of 6 integers. Get numbers from the input,
+//find and print the sum of the even numbers from the array:
+
+//Test Data:
+//Enter integer no.1:
+//4
+//Enter integer no.2:
+//3
+//Enter integer no.3:
+//7
+//Enter integer no.4:
+//3
+//Enter integer no.5:
+//2
+//Enter integer no.6:
+//8
+//Expected Output:
+//The result is: 14
+
+
+int[] numbers = new int[6];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"Enter integer no.{i + 1}:\n");
+    bool success = int.TryParse(Console.ReadLine(), out numbers[i]);
+    if (!success)
+    {
+        Console.WriteLine("Invalid input. Please enter a valid integer.");
+        i--;
+        continue;
+    }
+}
+foreach (int num in numbers)
+{
+    if(num %2 == 0)
+    {
+        sum += num;
+    }
+}
+Console.WriteLine($"The result is: {sum}");
---
+++ Class03 Homework/Code/StudentGroup/Program.cs
@@ -0,0 +1,48 @@
+﻿//Make a new console application called StudentGroup
+//Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+//Get a number from the console between 1 and 2 and print the students from that group in the console.
+//Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+//Test Data:
+//Enter student group: (there are 1 and 2 )
+//1
+//Expected Output:
+//The Students in G1 are:
+//Zdravko
+//Petko
+//Stanko
+//Branko
+//Trajko
+
+string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = { "Ana", "Mila", "Johanna", "Elena", "Ivana" };
+
+Console.WriteLine("Enter student group: (there are 1 and 2)");
+string input = Console.ReadLine();
+
+if(int.TryParse(input, out int groupNumber))
+{
+    switch (groupNumber)
+    {
+        case 1:
+            Console.WriteLine("The students in G1 are:");
+            foreach(string student in studentsG1)
+            {
+                Console.WriteLine(student);
+            }
+            break;
+        case 2:
+            Console.WriteLine("The students in G2 are:");
+            foreach(string student in studentsG2)
+            {
+                Console.WriteLine(student);
+            }
+            break;
+        default:
+            Console.WriteLine("invalid input. Please enter 1 or 2.");
+            break;
+    }   
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid number (1 or 2).");
+}
